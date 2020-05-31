82b25683b17e61699f357a916a6bfb472068a533
Pradeep Yadav <pradpyadav@gmail.com>
2020-05-30 20:01:26 -0400

    Created WebDemo repository for Jenkins

---
+++ jenkindemo2/src/main/java/com/luv2code/jenkinproject/Jenkindemo2Application.java
@@ -0,0 +1,13 @@
+package com.luv2code.jenkinproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkindemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkindemo2Application.class, args);
+	}
+
+}
---
+++ jenkindemo2/src/test/java/com/luv2code/jenkinproject/Jenkindemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.jenkinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkindemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
